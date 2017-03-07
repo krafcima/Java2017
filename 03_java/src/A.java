3208b1c102a0de3f63deb2cf776f030575d5df61
borovan <borovan@microstep-mis.com>
2017-03-07 21:46:24 +0100

    kod tretej prednasky

---
+++ 03_java/src/A.java
@@ -0,0 +1,9 @@
+class A {
+ A A(A A) {
+  A:
+  for (;;) {
+   if (A.A(A) == A) break A; 
+  }
+  return A;
+ }
+}
---
+++ 03_java/src/ZlaPostupnosti.java
@@ -0,0 +1,9 @@
+abstract 
+class ZlaPostupnosti extends Postupnost {
+		protected long delta;
+
+		ZlaPostupnosti(int _delta) {
+			   delta = _delta; prvy = 0;	
+		}
+}
+
